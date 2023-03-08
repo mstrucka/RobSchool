ff2137cd83edfb90a7c5ac018f37c44ec4315fc7
Believer11 <107124774+Believer11@users.noreply.github.com>
2023-03-08 10:01:02 +0100

    Function

--- School/src/Main.java
+++ School/src/Main.java
@@ -1,24 +1,58 @@
-import container.PersonContainer;
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 import model.Person;
 
 public class Main {
-    public static void main(String[] args) {
-        Person p = new Person("robike", 24);
-        PersonContainer.getInstance().addPersonByObject(p);
-        System.out.println(PersonContainer.getInstance().getAllPeople());
-        // TODO: všetko toto píšeme sem do mainu
-        //  1 list mien, aspoň 10 vecí hardcoded, takisto list čísel čo bude vek, 10 RANDOM čísel v ňom
-        //  2 for loop ktorý vytvorí 10 Persons, s tým že to meno a vek zoberie randomne z tých listov
-        //  3 vypísať AllPeople, bude treba asi toString
-        //  4 vyskúšať všetky metódy z Containera či fungujú tu v maine, štýlom že:
-        System.out.println("menim robikemu meno na sepetlak, robike pred zmenou: " + PersonContainer.getInstance().getPersonByName("robike"));
-        p.setName("sepetlak");
-        PersonContainer.getInstance().editPersonByObject(p);
-        System.out.println("Robike po zmene: " + PersonContainer.getInstance().getPersonByName("sepetlak"));
-        // TODO: keď budeš chápať ako funguje toto s Person a všetko ti pôjde, spravíš to isté so School ideálne bez pozerania do kódu
-        // TODO: a sám si tú School navrhneš aké metódy budú v tom kontajneri, ako si vytvoríš tú school, etc. precvičuj for, random, iterovanie listom
-
-        // BONUS: list bude len abeceda a to meno pre person/školu poskladáš z 5-8 písmen
-
+    public Main() {
     }
+
+    public static void main(String[] args) {
+        List<String> names = new ArrayList();
+        names.add("Robike");
+        names.add("Marek");
+        names.add("Juri");
+        names.add("Misko");
+        names.add("Martin");
+        names.add("Sepetlak");
+        names.add("Patres");
+        List<Integer> ages = new ArrayList();
+        ages.add(24);
+        ages.add(26);
+        ages.add(28);
+        ages.add(29);
+        ages.add(30);
+        ages.add(22);
+        ages.add(33);
+        new ArrayList();
+        List<Person> PersonContainer = new ArrayList(names.size());
+        System.out.println(PersonContainer);
+        Random generate = new Random();
+
+        for(int i = 0; i < 7; ++i) {
+            PersonContainer.add(new Person((String)names.get(generate.nextInt(names.size())), (Integer)ages.get(generate.nextInt(ages.size()))));
+        }
+
+        System.out.println(PersonContainer.toString());
+    }
+//    Person p = new Person("robike", 24);
+//        PersonContainer.getInstance().addPersonByObject(p);
+//        System.out.println(PersonContainer.getInstance().getAllPeople());
+    // TODO: všetko toto píšeme sem do mainu
+    //  1 list mien, aspoň 10 vecí hardcoded, takisto list čísel čo bude vek, 10 RANDOM čísel v ňom
+    //  2 for loop ktorý vytvorí 10 Persons, s tým že to meno a vek zoberie randomne z tých listov
+    //  3 vypísať AllPeople, bude treba asi toString
+    //  4 vyskúšať všetky metódy z Containera či fungujú tu v maine, štýlom že:
+//
+
+    // TODO: keď budeš chápať ako funguje toto s Person a všetko ti pôjde, spravíš to isté so School ideálne bez pozerania do kódu
+    // TODO: a sám si tú School navrhneš aké metódy budú v tom kontajneri, ako si vytvoríš tú school, etc. precvičuj for, random, iterovanie listom
+
+    // BONUS: list bude len abeceda a to meno pre person/školu poskladáš z 5-8 písmen
+
 }
